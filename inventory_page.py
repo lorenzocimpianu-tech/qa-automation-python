e4cc39a2e4bb887b2138bd4529d6f9e39020eb4b
cimpi-tech <lorenzocimpianu@gmail.com>
2026-06-09 13:56:27 +0200

    Add files via upload

---
+++ inventory_page.py
@@ -0,0 +1,32 @@
+class InventoryPage:
+    """Gestisce tutti gli elementi e le azioni della pagina catalogo prodotti."""
+
+    EXPECTED_URL_FRAGMENT = "inventory.html"
+    EXPECTED_TITLE = "Products"
+
+    def __init__(self, page):
+        self.page = page
+        self.title = page.locator("span.title")
+        self.cart_icon = page.locator(".shopping_cart_link")
+        self.cart_badge = page.locator(".shopping_cart_badge")
+
+    def is_loaded(self) -> bool:
+        return self.EXPECTED_URL_FRAGMENT in self.page.url
+
+    def get_title(self) -> str:
+        return self.title.text_content()
+
+    def add_product_to_cart(self, product_name: str):
+        """Aggiunge un prodotto al carrello cercandolo per nome."""
+        button_locator = self.page.locator(
+            f"[data-test='add-to-cart-{product_name.lower().replace(' ', '-')}']"
+        )
+        button_locator.click()
+
+    def get_cart_count(self) -> int:
+        if not self.cart_badge.is_visible():
+            return 0
+        return int(self.cart_badge.text_content())
+
+    def go_to_cart(self):
+        self.cart_icon.click()
